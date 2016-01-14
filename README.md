3e44a3c9f44c75ed99ca8b506cc4faf72302dc4b
fy <fy0@qq.com>
2016-01-14 20:45:29 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# pylite ver 0.0.1
+
+A simple, lightweight implementation of python3-like language.
+
+License：zlib
---
+++ src/shell.c
@@ -0,0 +1,11 @@
+﻿
+#include "stdio.h"
+
+#ifdef SHELL
+
+int main(int argc,char* argv[])
+{
+    return 0;
+}
+
+#endif
