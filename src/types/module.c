a1e94b619cf2b7cf7948177cefc3336db3917c09
fy <fy0@qq.com>
2016-03-26 00:03:09 +0800

    update

--- src/types/module.c
+++ src/types/module.c
@@ -1,11 +1,11 @@
 ﻿
 #include "module.h"
 
-PyLiteModuleObject* pylt_obj_module_new(PyLiteState *state, VecObject const_val, VecOpcode opcodes) {
+PyLiteModuleObject* pylt_obj_module_new(PyLiteState *state, PyLiteCodeSnippet *code) {
     PyLiteModuleObject *obj = pylt_realloc(NULL, sizeof(PyLiteModuleObject));
     obj->ob_type = PYLT_OBJ_TYPE_MODULE;
     obj->attrs = pylt_obj_table_new(state);
-    kv_shallowcopy(obj->const_val, const_val);
-    kv_shallowcopy(obj->opcodes, opcodes);
+    kv_shallowcopy(obj->const_val, code->const_val);
+    kv_shallowcopy(obj->opcodes, code->opcodes);
     return obj;
 }
--- src/types/module.h
+++ src/types/module.h
@@ -5,18 +5,18 @@
 #include "object.h"
 #include "../lib/kvec.h"
 
-typedef kvec_t(PyLiteObject*) VecObject;
-typedef kvec_t(uintptr_t*) VecOpcode;
-
+typedef struct PyLiteCodeSnippet {
+    kvec_t(PyLiteObject*) const_val;
+    kvec_t(uintptr_t) opcodes;
+} PyLiteCodeSnippet;
 
 typedef struct PyLiteModuleObject {
     PyLiteObject_HEAD;
     PyLiteTable *attrs;
-
     kvec_t(PyLiteObject*) const_val;
     kvec_t(uintptr_t) opcodes;
 } PyLiteModuleObject;
 
-PyLiteModuleObject* pylt_obj_module_new(PyLiteState *state, VecObject const_val, VecOpcode opcodes);
+PyLiteModuleObject* pylt_obj_module_new(PyLiteState *state, PyLiteCodeSnippet *code);
 
 #endif
