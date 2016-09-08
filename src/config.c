2103ab4b7a86135a7191857d1a6722030d318b85
fy <fy0@qq.com>
2016-09-09 03:41:04 +0800

    pylt_realloc fixed

--- src/config.c
+++ src/config.c
@@ -3,7 +3,13 @@
 #include "intp.h"
 
 void* pylt_realloc(PyLiteInterpreter *I, void* m, size_t osize, size_t nsize) {
-    void* ret = realloc(m, nsize);
-    if (I) I->mem_used = I->mem_used - osize + nsize;
-    return ret;
+    if (nsize == 0) {
+        free(m);
+        if (I) I->mem_used -= osize;
+        return NULL;
+    } else {
+        void* ret = realloc(m, nsize);
+        if (I) I->mem_used = I->mem_used - osize + nsize;
+        return ret;
+    }
 }
