d0ba4e01aea436637e0731f9a927d12b7c014510
fy <fy0@qq.com>
2017-02-20 00:54:31 +0800

    update

--- src/deps/utfconvert/utfconvert.c
+++ src/deps/utfconvert/utfconvert.c
@@ -1,9 +1,9 @@
 ﻿
 #include "UTFConvert.h"
 
-int uc_utf8_to_ucs4(const uint8_t *ostr, uint32_t *pbuf) {
+int uc_utf8_to_ucs4(const uint8_t *u8, int u8maxlen, uint32_t *pbuf) {
     static const unsigned int limits[] = { 0xFF, 0x7F, 0x7FF, 0xFFFF };
-    const unsigned char *s = (const unsigned char *)ostr;
+    const unsigned char *s = (const unsigned char *)u8;
     unsigned int c = s[0];
     unsigned int res = 0;  /* final result */
     if (c < 0x80)  /* ascii? */
@@ -11,6 +11,7 @@ int uc_utf8_to_ucs4(const uint8_t *ostr, uint32_t *pbuf) {
     else {
         int count = 0;  /* to count number of continuation bytes */
         while (c & 0x40) {  /* still have continuation bytes? */
+            if (u8maxlen && (count == u8maxlen)) return UC_RET_INVALID;
             int cc = s[++count];  /* read next byte */
             if ((cc & 0xC0) != 0x80)  /* not a continuation byte? */
                 return UC_RET_INVALID;  /* invalid byte sequence */
@@ -23,7 +24,7 @@ int uc_utf8_to_ucs4(const uint8_t *ostr, uint32_t *pbuf) {
         s += count;  /* skip continuation bytes read */
     }
     if (pbuf) *pbuf = res;
-    return (s - ostr) + 1;  /* +1 to include first byte */
+    return (s - u8) + 1;  /* +1 to include first byte */
 }
 
 int uc_ucs4_to_utf8(uint32_t wc, uint8_t *pbuf) /* n == 0 is acceptable */ {
@@ -47,25 +48,26 @@ int uc_ucs4_to_utf8(uint32_t wc, uint8_t *pbuf) /* n == 0 is acceptable */ {
     return count;
 }
 
-int uc_utf16le_to_ucs4(const uint16_t *u16, uint32_t *pbuf) {
+int uc_utf16le_to_ucs4(const uint16_t *u16, int u16maxlen, uint32_t *pbuf) {
     uint16_t w1 = u16[0];
     if (w1 >= 0xD800 && w1 <= 0xDFFF) {
         if (w1 < 0xDC00) {
+            if (u16maxlen) return UC_RET_INVALID;
             uint16_t w2 = u16[1];
             if (w2 >= 0xDC00 && w2 <= 0xDFFF) {
-                if (pbuf) pbuf = (uint32_t)(w2 & 0x03FF) + (((w1 & 0x03FF) + 0x40) << 10);
+                if (pbuf) *pbuf = (uint32_t)(w2 & 0x03FF) + (((w1 & 0x03FF) + 0x40) << 10);
                 return 2;
             }
         }
         return UC_RET_INVALID;
     } else {
-        if (pbuf) pbuf = (uint32_t)w1;
+        if (pbuf) *pbuf = (uint32_t)w1;
         return 1;
     }
 }
 
 int uc_ucs4_to_utf16le(uint32_t wc, uint16_t *pbuf) {
-    uint8_t *pb8 = pbuf;
+    uint8_t *pb8 = (uint8_t *)pbuf;
     if (!(wc >= 0xd800 && wc < 0xe000)) {
         if (wc < 0x10000) {
             pb8[0] = (uint8_t)wc;
@@ -84,20 +86,28 @@ int uc_ucs4_to_utf16le(uint32_t wc, uint16_t *pbuf) {
     return UC_RET_INVALID;
 }
 
-int uc_utf8_to_wchar(const uint8_t *ostr, wchar_t *pbuf) {
+int uc_utf8_to_wchar(const uint8_t *u8, int u8maxlen, wchar_t *pbuf, int *pbuf_used) {
     if (sizeof(wchar_t) == sizeof(uint16_t)) {
-        return uc_utf8_to_utf16le(ostr, (uint16_t*)pbuf);
+        uint32_t tmp;
+        int ret = uc_utf8_to_ucs4(u8, u8maxlen, &tmp);
+        if (ret <= 0) return ret;
+        *pbuf_used = uc_ucs4_to_utf16le(tmp, (uint16_t*)pbuf);
+        return ret;
     } else if (sizeof(wchar_t) == sizeof(uint32_t)) {
-        return uc_utf8_to_ucs4(ostr, (uint32_t*)pbuf);
+        *pbuf_used = 1;
+        return uc_utf8_to_ucs4(u8, u8maxlen, (uint32_t*)pbuf);
     }
     return UC_RET_INVALID;
 }
 
-int uc_wchar_to_utf8(wchar_t wc, uint8_t *pbuf) {
+int uc_wchar_to_utf8(wchar_t *wch, int wchlen, uint8_t *pbuf) {
     if (sizeof(wchar_t) == sizeof(uint16_t)) {
-        return uc_utf16le_to_utf8((uint16_t)wc, pbuf);
+        uint32_t tmp;
+        int ret = uc_utf16le_to_ucs4((const uint16_t*)wch, wchlen, &tmp);
+        if (ret <= 0) return ret;
+        return uc_ucs4_to_utf8(tmp, pbuf);
     } else if (sizeof(wchar_t) == sizeof(uint32_t)) {
-        return uc_ucs4_to_utf8((uint32_t)wc, pbuf);
+        return uc_ucs4_to_utf8((uint32_t)*wch, pbuf);
     }
     return UC_RET_INVALID;
 }
